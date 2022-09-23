7836748833e846c62202dc2d30d424375e2001e7
Tadashi Matsuura <4456384+tadashi1105@users.noreply.github.com>
2022-09-23 13:12:51 +0900

    Initial commit

---
+++ src/components/Square.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { SquareType } from '../types';
+
+type SquareProps = {
+  value: SquareType;
+  onClick: () => void;
+};
+
+const Square: React.FC<SquareProps> = ({ value, onClick }) => {
+  return (
+    <button className="square" onClick={onClick}>
+      {value}
+    </button>
+  );
+};
+
+export default Square;
